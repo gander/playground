adc71345fa8d610dd76112f079397124931530e2
Adam Gąsowski <adam.gasowski@gander.pl>
2022-11-26 13:18:24 +0100

    init monkey playground

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  padding: 10px;
+  border: 3px dotted #ff32cf;
+  width: auto;
+}
+</style>
